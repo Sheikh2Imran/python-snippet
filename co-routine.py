7e2462e17f3708d6ca3440c2b900630739306260
Md Imran Sheikh <imranjustcse@gmail.com>
2021-03-23 11:56:15 +0600

    Update co-routine.py

--- co-routine.py
+++ co-routine.py
@@ -1,41 +1,15 @@
-# producer / initial source
-def producer(sentence, next_coroutine):
-    tokens = sentence.split(" ")
-    for token in tokens:
-        next_coroutine.send(token)
-    
-    next_coroutine.close()
-
-
-# intermediat co-routine
-def pattern_filter(pattern="ing", next_coroutine=None):
-    try:
-        while True:
-            token = (yield)
-            if pattern in token:
-                next_coroutine.send(token)
-                
-    except GeneratorExit:
-        print("Done with filtering!")
-
-
-# sink
-def print_token():
-    print("I'm sink, i'll print tokens") 
-    try:
-        while True:
-            token = (yield)
-            print(token)
-            
-    except GeneratorExit:
-        print("Done with printing!")
-
-
-if __name__ == "__main__":
-    pt = print_token()
-    pt.__next__()
-    pf = pattern_filter(next_coroutine=pt)
-    pf.__next__()
-    
-    sentence = "Bob is running behind a fast moving car"
-    producer(sentence, pf)
+def print_name(prefix): 
+    print("Searching prefix:{}".format(prefix)) 
+    try :  
+        while True: 
+                name = (yield) 
+                if prefix in name: 
+                    print(name) 
+    except GeneratorExit: 
+            print("Closing coroutine!!") 
+  
+corou = print_name("Dear") 
+corou.__next__() 
+corou.send("Atul") 
+corou.send("Dear Atul") 
+corou.close()
